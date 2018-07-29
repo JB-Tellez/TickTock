d63947a56dc004efba9d7f10ee603876b99e0841
JB Tellez <jb.tellez@gmail.com>
2018-07-29 14:03:14 -0700

    fixes clock import

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Clock from './Clock';
 
 export default class App extends React.Component {
   constructor(props) {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Clock from "./components/Clock";
 import App from "./components/App";
 import "./styles.css";
 
